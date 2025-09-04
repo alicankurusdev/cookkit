e87c3d919d8516b201d1bb26f9122a068e8dfcc5
AlicanKurus <alicankurus.dev@gmail.com>
2025-09-04 17:34:54 +0200

    first commit

---
+++ cookclient/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+const a = 5
+const b = 4
+const c = 4
+const d = 8
+
+const e=(a,b,r)=>{
+  let t=  a*2;
+    let y=b+3;
+   let z= c-1
+ return t+y+z
+}
+
+const h = e(a,d)
+
+  return (
+    <div>
+    </div>
+      
+  )
+}
+
+export default App
