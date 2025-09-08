23464808ffbcc8b7f78203b1f2f20d3832fc8826
AlicanKurus <alicankurus.dev@gmail.com>
2025-09-08 18:50:00 +0200

    db connected cloud api is up

--- cookserver/index.js
+++ cookserver/index.js
@@ -9,3 +9,51 @@ const app=express()
 
 // Nested Query
 app.set("query parser", "extended");
+
+//dotenv
+require("dotenv").config()
+const PORT = process.env?.PORT || 8000
+
+//DB Connect
+
+const {dbConnection}=require("./src/configs/dbConnection");
+ dbConnection()
+
+// JSON config:
+app.use(express.json());
+
+
+// Cookie-Session:
+app.use(require('cookie-session')({
+    keys: [process.env.SECRET_KEY || "default-secret"], // burası şart
+}));
+
+
+// Query Handler:
+app.use(require('./src/middlewares/queryHandler'));
+
+// Authentication:
+app.use(require('./src/middlewares/auth'));
+
+// Routes
+
+// Home Path
+app.all('/', (req, res) => {
+
+    res.status(200).send({
+        error: false,
+        message: 'Welcome to Personnel API Service',
+        session: req.session
+    });
+});
+
+/* ------------------------------------------------------- */
+// ErrorHandler:
+app.use(require('./src/middlewares/errorHandler'))
+
+// RUN SERVER:
+app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
+
+/* ------------------------------------------------------- */
+// Syncronization (must be in commentLine):
+// require('./src/helpers/sync')()
---
+++ cookserver/src/middlewares/queryHandler.js
@@ -0,0 +1,15 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESS - RECIPE API
+------------------------------------------------------- */
+// app.use(errorHandler):
+
+module.exports = (err, req, res, next) => {
+    // console.log(err.statusCode);
+    return res.status(err?.statusCode || 500).send({
+        error: true,
+        message: err.message,
+        cause: err.cause,
+        body: req.body
+    });
+}
