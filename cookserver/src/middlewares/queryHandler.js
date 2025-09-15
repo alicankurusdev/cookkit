0cb3ff7fd3fc4ef2dac74ac240f57eecdc79aaff
AlicanKurus <alicankurus.dev@gmail.com>
2025-09-15 18:42:16 +0200

    queryHandler mw is done

--- cookserver/src/middlewares/queryHandler.js
+++ cookserver/src/middlewares/queryHandler.js
@@ -1,15 +1,59 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
-    EXPRESS - RECIPE API
+Cookkit-recipeAPP-users-controller
 ------------------------------------------------------- */
-// app.use(errorHandler):
+//* QUERY HANDLER MIDDLEWARE;
 
-module.exports = (err, req, res, next) => {
-    // console.log(err.statusCode);
-    return res.status(err?.statusCode || 500).send({
-        error: true,
-        message: err.message,
-        cause: err.cause,
-        body: req.body
-    });
+module.exports = async (req, res, next) => {
+
+   //Filter
+    const filter = req.query?.filter || {}
+    //Search
+    const search = req.query?.search || {}
+    for (let key in search)
+        search[key] = { $regex: search[key] }
+     //Sort
+    const sort = req.query?.sort || {}
+    //LIMIT
+    let limit = Number(req.query?.limit)
+    limit = limit > 0 ? limit : Number(process.env?.PAGE_SIZE) || 20
+    // PAGE
+    let page = Number(req.query?.page)
+    page = page > 0 ? page : 1
+    // SKIP
+    let skip = Number(req.query?.skip)
+    skip = skip > 0 ? skip : (page - 1) * limit
+    // GetModelList
+    res.getModelList = async (Model, customFilter = {}, populate = null) => {
+        return await Model.find({ ...filter, ...search, ...customFilter }).sort(sort).skip(skip).limit(limit).populate(populate);
+    };
+    // Details
+    res.getModelListDetails = async function (Model, customFilter = {}) {
+
+        const data = await Model.find({ ...filter, ...search, ...customFilter });
+
+        let details = {
+            filter,
+            search,
+            sort,
+            skip,
+            limit,
+            page,
+            totalRecords: data.length,
+            pages: {
+                previos: (page > 1 ? page - 1 : false),
+                current: page,
+                next: page + 1,
+                total: Math.ceil(data.length / limit)
+            }
+        };
+
+        if (details.pages.next > details.pages.total) details.pages.next = false;
+        if (details.totalRecords <= limit) details.pages = false
+
+        return details
+    }
+
+
+    next()
 }
